f669f04a1428cac68b83c83ef711c7f86e1e8c58
Matthew Chavis <122698547+matthewchavis8@users.noreply.github.com>
2026-01-01 19:48:33 -0600

    Notes for helper.h

--- helper.h
+++ helper.h
@@ -1,5 +1,19 @@
+// NOTE:
+//  Missing a header guard above _HELPER_H__
+
+
+// NOTE:
+//   Question just wondering why use a template here.
+//   Templates add a lot of bloat to C++ files due to the Compiler generating
+//   many many different variations of your function or obj. A good rule of thumb is keep it simple
+//   C++ offers a lot of fun and cool fancy features. But there is pros and cons with everything. In my opinion the
+//   use of templates here overcomplicates things you are always reading a numerical value, why do we need to generate
+//   a string, double, float, string_view, etc version of this it just adds more size to your binary
+//
+//
 template<typename T>
 void readInput(T& var){
+    // NOTE: What happens if you can not read the value I Suggest throw an error or some sort of error
     while(!(std::cin >> var)){
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
